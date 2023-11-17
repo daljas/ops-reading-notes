1c365960bbe17b7bfede7a2da7670aacd5fadec5
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-16 20:51:19 -0500

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,8 @@
+# Install Slack
+Invoke-WebRequest -Uri "https://slack.com/downloads/windows" -OutFile "$env:USERPROFILE\Downloads\SlackSetup.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\SlackSetup.exe"
+
+# Install Zoom
+Invoke-WebRequest -Uri "https://zoom.us/client/latest/ZoomInstaller.exe" -OutFile "$env:USERPROFILE\Downloads\ZoomInstaller.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\ZoomInstaller.exe"
+
+# Install AnyDesk
+Invoke-WebRequest -Uri "https://download.anydesk.com/AnyDesk.exe" -OutFile "$env:USERPROFILE\Downloads\AnyDesk.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\AnyDesk.exe"
