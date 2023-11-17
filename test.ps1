1b91985a533bdc36790ee23fbf7b4010abc4e4a8
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-16 20:54:35 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,8 +1,8 @@
 # Install Slack
-Invoke-WebRequest -Uri "https://slack.com/downloads/windows" -OutFile "$env:USERPROFILE\Downloads\SlackSetup.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\SlackSetup.exe"
+Invoke-WebRequest -Uri "https://slack.com/downloads/windows" -OutFile "$env:USERPROFILE\Downloads\SlackSetup.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:admin\Downloads\SlackSetup.exe"
 
 # Install Zoom
-Invoke-WebRequest -Uri "https://zoom.us/client/latest/ZoomInstaller.exe" -OutFile "$env:USERPROFILE\Downloads\ZoomInstaller.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\ZoomInstaller.exe"
+Invoke-WebRequest -Uri "https://zoom.us/client/latest/ZoomInstaller.exe" -OutFile "$env:USERPROFILE\Downloads\ZoomInstaller.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:admin\Downloads\ZoomInstaller.exe"
 
 # Install AnyDesk
-Invoke-WebRequest -Uri "https://download.anydesk.com/AnyDesk.exe" -OutFile "$env:USERPROFILE\Downloads\AnyDesk.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:USERPROFILE\Downloads\AnyDesk.exe"
+Invoke-WebRequest -Uri "https://download.anydesk.com/AnyDesk.exe" -OutFile "$env:USERPROFILE\Downloads\AnyDesk.exe" -UseBasicParsing; Start-Process -Wait -FilePath "$env:admin\Downloads\AnyDesk.exe"
